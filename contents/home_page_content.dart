7b5a7a25da96a08cba51db502e0ede72f2103369
MD. Sajid Hossain <69637820+sajidrec@users.noreply.github.com>
2025-10-30 09:18:47 +0600

    fix issue

--- contents/home_page_content.dart
+++ contents/home_page_content.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold());
+    return Scaffold(body:SafeArea(child:SingleChildScrollView(child:Column([]))));
   }
 }
 
